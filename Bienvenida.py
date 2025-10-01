d3ef3e635d50a5c163398b3594c2a5835e3902f8
Rigo-cerino <cerino.rigoberto@tec.mx>
2025-10-01 17:53:46 -0600

    Función suma

--- Bienvenida.py
+++ Bienvenida.py
@@ -1,3 +1,13 @@
 print("Hola de nuevo, este es un repositorio creado desde Github")
 
 print("Hola de regreso")
+
+a = 89
+b = 52
+c = 23
+
+def suma():
+    print("Suma total: ", a + b + c)
+
+suma()
+    
